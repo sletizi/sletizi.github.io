c93efbb49cc42aeb48429f6db3bb91055a84c09c
Simone Letizi <50908915+sletizi@users.noreply.github.com>
2022-09-09 17:55:14 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/sletizi.github.io/" : "/",
+  publicPath: "/",
   chainWebpack: config => {
     config.module
       .rule('vue')
