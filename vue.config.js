426c4e5ff9a401528bec7ee0d2ab5b10ab014b22
Simone Letizi <s.letizi@gmail.com>
2022-09-09 18:36:42 +0200

    trying fix routing in subpages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: "/",
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/sletizi.github.io/'
+  : '/',
   chainWebpack: config => {
     config.module
       .rule('vue')
