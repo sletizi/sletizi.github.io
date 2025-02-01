fbe99cabfe81742b8943445b9a18d5c616d75e80
Simone Letizi <s.letizi@gmail.com>
2025-02-01 19:52:24 +0100

    new images

--- src/components/ProfessionalInterestHome.vue
+++ src/components/ProfessionalInterestHome.vue
@@ -54,20 +54,21 @@ export default {
         return {
             interests: [
             {
+                icon: 'https://www.projest.com/wp-content/uploads/2024/09/Software-di-business-intelligence-1024x683.jpg',
+                title: 'Business <br/> Intelligence',
+                text: 'Da riempire',
+            },
+            {
+                icon: 'https://files.oaiusercontent.com/file-FyJ1J4ZAdBz5DbnvYfXu19?se=2025-02-01T18%3A51%3A53Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D07d24255-4e87-4ee4-a215-ac67ba6a282c.webp&sig=W47IXsRVbY/LR%2BfYM6ONIXBaeUcq8EKBTh/efvhFtpI%3D',
+                title: 'Data <br/> Engineering',
+                text: 'Da riempire',
+            },
+            {
                 icon: 'https://faithandleadership.com/sites/default/files/field/image/DavidSmith-teaching_m.png',
                 title: 'Insegnamento<br/> & <br/> didattica',
                 text: 'Da riempire',
             },
-            {
-                icon: 'https://img.deusm.com/informationweek/November19/datascience-pdusit-stock.jpg',
-                title: 'Data <br/> Science',
-                text: 'Da riempire',
-            },
-            {
-                icon: 'https://rntechltd.files.wordpress.com/2015/02/scala-code.png',
-                title: 'AI <br/> & <br/> programmazione',
-                text: 'Da riempire',
-            }]
+          ]
         }      
     }
 }
