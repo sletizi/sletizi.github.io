2d2b3e6fa592c34cbed896ad81b781d30ddfc36c
Simone Letizi <50908915+sletizi@users.noreply.github.com>
2025-04-19 08:57:37 +0200

    fix logo in bar not circular

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,9 +6,10 @@
           class="mr-3 ma-2"
           color="grey lighten-5"
           size="60"
+          style="overflow: hideen;aspect-ratio: 1/1;"
         >
           <v-img
-            contain
+            cover
             src="../assets/logos/logo.png"
             @click="goHome"
             style="max-height: 50px;"
