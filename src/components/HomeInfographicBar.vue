dfe9926828ac515c2de0c27a92f6a1040143696f
Simone Letizi <50908915+sletizi@users.noreply.github.com>
2025-05-18 23:05:39 +0200

    changed home infographic

--- src/components/HomeInfographicBar.vue
+++ src/components/HomeInfographicBar.vue
@@ -1,61 +1,92 @@
 <template>
     <v-row no-gutters>
-        <v-img
+      <v-img
         :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+        :max-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
         src="../assets/room.jpeg"
-        >
+        class="position-relative"
+      >
         <v-theme-provider dark>
-            <v-container fill-height >
+          <v-container fill-height>
             <v-row
-                align="start"
-                justify="start"
-                class="white--text"
-
+              align="start"
+              justify="start"
+              class="white--text"
             >
-                <v-col
+              <v-col
                 class="white--text text-center"
                 cols="12"
                 tag="h1"
-                >
-                <v-row>
-                    <span
-                        class="font-weight-light"
-                        :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"                   
-                    >
-                        "Cerco verità nei dati e silenzi tra le vette: entrambi raccontano più di quanto sembri."
-
-
-                    </span>
-                </v-row>
-                <br>
+              >
                 <v-row
-                    align="center"
-                    justify="center">   
-                    <v-btn
-                    class="align-self-end"
+                  align="center"
+                  justify="center"
+                >
+                  <v-btn
+                    icon
                     fab
                     outlined
+                    large
+                    class="scroll-down"
                     @click="$vuetify.goTo('#about-me')"
-                    >
-                    <v-icon>mdi-chevron-double-down</v-icon>
-                    </v-btn>
+                    :class="{ 'at-bottom': movedToBottom }"
+                  >
+                    <v-icon large>mdi-chevron-down</v-icon>
+                  </v-btn>
                 </v-row>
-                </v-col>
+              </v-col>
             </v-row>
-            <v-row
-                align="center"
-                justify="center">
-               
-            </v-row>    
-            </v-container>
+          </v-container>
         </v-theme-provider>
-        </v-img>
+      </v-img>
     </v-row>
-</template>
+  </template>
+  
 
-<script>
-export default {
-    components:{
-    }
+  <script>
+  export default {
+    data() {
+      return {
+        movedToBottom: false,
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.movedToBottom = true;
+      }, 2000); // dopo 2 secondi la freccia si sposta
+    },
+  };
+  </script>
+  
+  <style>
+  .scroll-down {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 50%;
+  transition: bottom 1s ease;
+  color: white;
+  z-index: 10;
 }
-</script>
+
+/* Quando arriva in fondo */
+.scroll-down.at-bottom {
+  bottom: 12vh;
+  animation: bounce 2s infinite;
+}
+
+/* Animazione bounce */
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateX(-50%) translateY(0);
+  }
+  40% {
+    transform: translateX(-50%) translateY(-10px);
+  }
+  60% {
+    transform: translateX(-50%) translateY(-5px);
+  }
+}
+
+  
+</style>
